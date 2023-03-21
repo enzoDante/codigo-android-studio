092df02a3e0e24bdce169fadd0eecf916a35588d
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2023-03-21 16:21:20 -0300

    projeto bimestral

---
+++ ProjetoHelio1bi/app/src/main/java/com/example/projetohelio1bi/MainActivity.kt
@@ -0,0 +1,117 @@
+package com.example.projetohelio1bi
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.marginBottom
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        carregarProdutos()
+
+    }
+    fun carregarProdutos(){
+        println("testataeaeteat=======")
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://helioesperidiao.com/api.php"
+        val requestBody = "id=1" + "&msg=test_msg"
+        val stringReq : StringRequest =
+            object : StringRequest(Method.POST, url,
+                Response.Listener { response ->
+                    val linearProdutos: LinearLayout = findViewById(R.id.produtos)
+                    var resposta = response.toString()
+                    val array = JSONArray(resposta)
+                    val tamanho =array.length()
+                    for (i in 0 until tamanho ) {
+                        val item: JSONObject = array.getJSONObject(i) // recupera o objeto na posição dentro do array
+                        var idProduto = item.get("id").toString()
+                        var type = item.get("type").toString();
+                        var nome = item.get("nome").toString();
+                        var desc = item.get("desc").toString();
+                        var qtd = item.get("qtd").toString();
+                        var preco = item.get("preco").toString();
+                        var img = item.get("img").toString();
+                        criarProdutosDinamicos(idProduto, type, nome, desc, qtd, img)
+                        /*var novoTextView = TextView(this)
+
+                        novoTextView.layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                        novoTextView.text = idProduto + " - " +type
+                        linearProdutos.addView(novoTextView)*/
+                        println(idProduto + " -teste " +type)
+                    }
+                },
+                Response.ErrorListener { error ->
+                    Log.d("API", "error => $error")
+                }
+            ){
+                override fun getBody(): ByteArray {
+                    return requestBody.toByteArray(Charset.defaultCharset())
+                }
+            }
+        queue.add(stringReq)
+    }
+
+    fun criarProdutosDinamicos(idProduto: String, type: String, nome:String, desc:String, qtd:String, img:String){
+        val linearProdutos: LinearLayout = findViewById(R.id.produtos)
+        linearProdutos.scrollY
+        var bloco = LinearLayout(this)
+
+        //bloco.marginBottom
+        bloco.orientation = LinearLayout.VERTICAL
+        bloco.setBackgroundColor(Color.parseColor("#D6D6D6"))
+
+        bloco.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+
+        var novoTextView = TextView(this)
+
+        novoTextView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        novoTextView.text = idProduto + " ${nome} " +type + "\n ${desc}"
+
+        val adicionar = Button(this)
+        adicionar.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        adicionar.text = "Adicionar pedido"
+
+        bloco.addView(novoTextView)
+        bloco.addView(adicionar)
+
+        linearProdutos.addView(bloco)
+        var espaco = TextView(this)
+
+        espaco.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        espaco.text = "   "
+        espaco.height = 100
+        linearProdutos.addView(espaco)
+
+    }
+
+}
